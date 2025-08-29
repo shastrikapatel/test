f22340328cbe970df92a27880cd47a90ecfa85a1
shastrikapatel <shastrikapatel24@gmail.com>
2025-08-29 16:27:59 +0530

    new

--- api/index.js
+++ api/index.js
@@ -51,6 +51,6 @@ app.get("/", (req, res) => {
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-}
+}    
 
 module.exports = app;
